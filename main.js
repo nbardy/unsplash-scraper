54131ff4712f2ed55d8167f3f109ee77b9a29e79
Constantine Gololobov <dev@gololobov.dev>
2021-08-02 16:33:18 +0200

    Clean Up

--- main.js
+++ main.js
@@ -1,51 +1,31 @@
 const Apify = require('apify')
-
-let collection = "demo"
-
-const getURLs = async () => {
-	try {
-		const input = await Apify.getInput()
-		const keyword = input.keyword.replace(/\s+/g, '-').trim().toLowerCase()
-		collection = keyword
-		let url = `https://unsplash.com/napi/search/photos?query=${keyword}&per_page=30`
-		if(input.color !== 'any') url += `&color=${input.color}`		
-		const response = await Apify.utils.requestAsBrowser({url})
-		const body = JSON.parse(response.body)
-		const totalPages = body.total_pages
-		let urls = []
-		for(let page = 1; page <= totalPages; page++) {
-			urls.push({url: `${url}&page=${page}`})
-		}
-		return urls
-	} catch(error) {
-		console.error(error)
-	}
-}
+const { log, requestAsBrowser } = Apify.utils
 
 
 Apify.main(async () => {
-	let results = {}
 	try {
-		const store = await Apify.openKeyValueStore('photos')
-		const requestList = await Apify.openRequestList('start-urls', await getURLs())
-		const crawler = new Apify.BasicCrawler({
-			requestList,
-			handleRequestFunction: async ({ request }) => {
-				console.log(`Request: ${request.url}`)
-				const response = await Apify.utils.requestAsBrowser(request)
-				const body = JSON.parse(response.body)
-				body.results.forEach(image => {
-					results[image.id] = image
-				})	
+		const { keyword, color } = await Apify.getInput()
+		const query = keyword.trim().toLowerCase().replace(/\s+/g, '-') // Need to Fix: Normalize query
+		
+		// Collect photos
+		let results = [], page = 1, completed
+		while(!completed) {
+			const url = `https://unsplash.com/napi/search/photos?query=${query}&color=${color}&per_page=30&page=${page++}`
+			let response = await requestAsBrowser({url})
+			let body = JSON.parse(response.body)
+			if(!body.results.length) completed = true
+			else {
+				log.info(`Scraping: ${url}`)
+				body.results.forEach(image => results.push(image))
 			}
-		})
-		await crawler.run()
-		await store.setValue(collection, results )
+		}
+
+		// Save photos
+		const store = await Apify.openKeyValueStore('photos')
+		await store.setValue(`${query}__${color}`, results )
+		log.info(`${results.length} photos processed`)
+
 	} catch(error) {
-		console.error(error)
-	} finally {
-		// const used = process.memoryUsage().heapUsed / 1024 / 1024
-		// console.info('\x1b[32m', `Memory: ${used.toFixed(2)} MB`)
-		results = null
+		log.error(error)
 	}
 })
