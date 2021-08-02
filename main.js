d491d9c031ff5de6fcc4e0e83df649027798622c
Constantine Gololobov <dev@gololobov.dev>
2021-08-02 12:18:42 +0200

    Base

---
+++ main.js
@@ -0,0 +1,51 @@
+const Apify = require('apify')
+
+
+const getURLs = async () => {
+	try {
+		const input = await Apify.getInput()
+		const query = input.query.replace(/\s+/g, '-')
+		let url = `https://unsplash.com/napi/search/photos?query=${query}&per_page=30`
+		if(input.color) url += `&color=${input.color}`
+		
+		const response = await Apify.utils.requestAsBrowser({url})
+		const body = JSON.parse(response.body)
+		const totalPages = body.total_pages
+		console.log(`Total: ${totalPages}`)
+		let urls = []
+		for(let page = 1; page <= totalPages; page++) {
+			urls.push({url: `${url}&page=${page}`})
+		}
+		return urls
+	} catch(error) {
+		console.error(error)
+	}
+}
+
+
+Apify.main(async () => {
+	let results = {}
+	try {
+		const store = await Apify.openKeyValueStore('photos')
+		const requestList = await Apify.openRequestList('start-urls', await getURLs())
+		const crawler = new Apify.BasicCrawler({
+			requestList,
+			handleRequestFunction: async ({ request }) => {
+				console.log(`Processing ${request.url}...`)
+				const response = await Apify.utils.requestAsBrowser(request)
+				const body = JSON.parse(response.body)
+				body.results.forEach(image => {
+					results[image.id] = image
+				})	
+			}
+		})
+		await crawler.run()
+		await store.setValue('demo', results )
+	} catch(error) {
+		console.error(error)
+	} finally {
+		const used = process.memoryUsage().heapUsed / 1024 / 1024
+		console.info('\x1b[32m', `Memory: ${used.toFixed(2)} MB`)
+		results = null
+	}
+})
