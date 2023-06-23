a9256ac5d9e7efaf7238877bf9e37b56762c06b8
Nicholas Bardy <nicholasbardy@gmail.com>
2023-06-23 13:04:50 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const Apify = require('apify');
-
 const { log, requestAsBrowser } = Apify.utils;
 
 Apify.main(async () => {
@@ -8,51 +7,51 @@ Apify.main(async () => {
         const { keywords, orientation, color } = await Apify.getInput();
         const keywordList = keywords.split(';').map(kw => kw.trim().toLowerCase().replace(/\s+/g, '-')); // Normalize queries
 
+        // Create a RequestQueue
+        const requestQueue = await Apify.openRequestQueue();
+
         for (const query of keywordList) {
             let url = `https://unsplash.com/napi/search/photos?query=${query}&per_page=30`;
             if (orientation !== 'any') url += `&orientation=${orientation}`;
             if (color !== 'any') url += `&color=${color}`;
 
-            // Generate List
-            const getRequestList = async () => {
+            // Generate Queue
+            const addToQueue = async () => {
                 try {
-                    log.info('Generating Requests List...');
+                    log.info('Adding to Queue...');
                     const response = await requestAsBrowser({ url });
                     const body = JSON.parse(response.body);
                     if (body.errors) throw body.errors;
                     const totalPages = body.total_pages;
-                    const urls = [];
                     for (let page = 1; page <= totalPages; page++) {
-                        urls.push(`${url}&page=${page}`);
+                        await requestQueue.addRequest({ url: `${url}&page=${page}` });
                     }
-                    log.info(`Generated ${urls.length} URLs.`);
-                    return urls;
+                    log.info(`Generated ${totalPages} URLs.`);
                 } catch (error) {
-                    throw new Error(`getRequestList: ${error}`); // Need to Fix Errors handling
+                    throw new Error(`addToQueue: ${error}`);
                 }
             };
 
-            // Crawl the URLs
-            const requestList = new Apify.RequestList({
-                sources: await getRequestList(),
-                persistStateKey: 'urls',
-            });
-            await requestList.initialize();
-            const photos = [];
-            const crawler = new Apify.BasicCrawler({
-                requestList,
-                handleRequestFunction: async ({ request }) => {
-                    log.info(`Processing: ${request.url}`);
-                    let { body } = await requestAsBrowser(request);
-                    body = JSON.parse(body);
-                    body.results.forEach((photo) => photos.push(photo));
-                },
-            });
+            await addToQueue();
+        }
 
-            await crawler.run();
+        // Crawl the URLs
+        const photos = [];
+        const crawler = new Apify.BasicCrawler({
+            requestQueue,
+            handleRequestFunction: async ({ request }) => {
+                log.info(`Processing: ${request.url}`);
+                let { body } = await requestAsBrowser(request);
+                body = JSON.parse(body);
+                body.results.forEach((photo) => photos.push(photo));
+            },
+        });
 
-            // Save photos
-            const store = await Apify.openKeyValueStore('unsplash');
+        await crawler.run();
+
+        // Save photos
+        const store = await Apify.openKeyValueStore('unsplash');
+        for (const query of keywordList) {
             await store.setValue(`${query}__${color}__${orientation}`, photos);
             log.info(`${photos.length} photos processed for keyword: ${query}`);
         }
